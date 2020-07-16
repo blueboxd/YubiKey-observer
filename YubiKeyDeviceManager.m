8af5f237437f7d3e42d4ba825c2c30d34752bd10
bluebox <bluebox@iMac-Pro.local>
2020-07-17 01:40:09 +0900

    get YubiKey properties via PCSC test

--- YubiKeyDeviceManager.m
+++ YubiKeyDeviceManager.m
@@ -9,8 +9,11 @@
 #import "YubiKeyDeviceManager.h"
 
 #include <IOKit/usb/IOUSBLib.h>
+#include <IOKit/hid/IOHIDLib.h>
 #include <IOKit/hid/IOHIDKeys.h>
+
 #include <PCSC/winscard.h>
+#include <PCSC/wintypes.h>
 
 NSNotificationName YubiKeyDeviceManagerKeyInsertedNotificationKey = @"YubiKeyDeviceManagerKeyInsertedNotificationKey";
 NSNotificationName YubiKeyDeviceManagerKeyRemovedNotificationKey = @"YubiKeyDeviceManagerKeyRemovedNotificationKey";
@@ -29,6 +32,116 @@ static YubiKeyDeviceManager *gSelf;
 	gSelf = self;
 	_devices = [NSMutableDictionary new];
 	return self;
+}
+
+#define CHECK(f, rv) \
+ if (SCARD_S_SUCCESS != rv) \
+ { \
+  printf(f ": %s\n", pcsc_stringify_error(rv)); \
+  return; \
+ }
+
+void sendAPDU(SCARDHANDLE hCard, SCARD_IO_REQUEST pioSendPci, BYTE apdu[], DWORD size) {
+	LONG rv;
+	DWORD dwRecvLength;
+	BYTE pbRecvBuffer[258];
+	
+	printf("request: ");
+	for(unsigned int i=0; i<size; i++)
+		printf("%02X ", apdu[i]);
+	printf("\n");
+
+	dwRecvLength = sizeof(pbRecvBuffer);
+	rv = SCardTransmit(hCard, &pioSendPci, apdu, size,
+					   NULL, pbRecvBuffer, &dwRecvLength);
+	CHECK("SCardTransmit", rv)
+	
+	printf("response: ");
+	for(unsigned int i=0; i<dwRecvLength; i++)
+		printf("%02X ", pbRecvBuffer[i]);
+	printf("\n");
+
+}
+
+- (void) awakeFromNib {
+	LONG rv;
+	
+	SCARDCONTEXT hContext;
+	LPTSTR mszReaders;
+	SCARDHANDLE hCard;
+	DWORD dwReaders, dwActiveProtocol;
+	
+	SCARD_IO_REQUEST pioSendPci;
+	
+	//							  cls   ins   p1    p2    lc    body -  
+	BYTE cmdSelectOTP[] =		{ 0x00, 0xA4, 0x04, 0x00, 0x08, 0xA0, 0x00, 0x00, 0x05, 0x27, 0x20, 0x01, 0x01 };
+	BYTE cmdSelectPIV[] =		{ 0x00, 0xA4, 0x04, 0x00, 0x05, 0xA0, 0x00, 0x00, 0x03, 0x08 };
+	BYTE cmdSelectMGR[] =		{ 0x00, 0xA4, 0x04, 0x00, 0x08, 0xA0, 0x00, 0x00, 0x05, 0x27, 0x47, 0x11, 0x17 };
+	BYTE cmdPIVGetSerial[] =	{ 0x00, 0xf8, 0x00, 0x00 };
+	BYTE cmdPIVGetVersion[] = 	{ 0x00, 0xfd, 0x00, 0x00 };
+	BYTE cmdGetConfig[] = 		{ 0x00, 0x1d, 0x00, 0x00 };
+	BYTE cmdOTPGetSerial[] = 	{ 0x00, 0x01, 0x10, 0x00, 0x00 };
+	BYTE cmdGetUID[] = 			{ 0xFF, 0xCA, 0x00, 0x00, 0x00 };
+	
+	
+	
+	rv = SCardEstablishContext(SCARD_SCOPE_SYSTEM, NULL, NULL, &hContext);
+	CHECK("SCardEstablishContext", rv)
+	
+#ifdef SCARD_AUTOALLOCATE
+	dwReaders = SCARD_AUTOALLOCATE;
+	
+	rv = SCardListReaders(hContext, NULL, (LPTSTR)&mszReaders, &dwReaders);
+	CHECK("SCardListReaders", rv)
+#else
+	rv = SCardListReaders(hContext, NULL, NULL, &dwReaders);
+	CHECK("SCardListReaders", rv)
+	
+	mszReaders = calloc(dwReaders, sizeof(char));
+	rv = SCardListReaders(hContext, NULL, mszReaders, &dwReaders);
+	CHECK("SCardListReaders", rv)
+#endif
+	printf("reader name: %s\n", mszReaders);
+	
+	rv = SCardConnect(hContext, mszReaders, SCARD_SHARE_SHARED,
+					  SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1, &hCard, &dwActiveProtocol);
+	CHECK("SCardConnect", rv)
+
+//#define 	SCARD_CLASS_VENDOR_INFO   1
+//#define 	SCARD_ATTR_VALUE(Class, Tag)   ((((ULONG)(Class)) << 16) | ((ULONG)(Tag)))
+//#define 	SCARD_ATTR_VENDOR_IFD_SERIAL_NO   SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_INFO, 0x0103)
+//#define 	SCARD_ATTR_VENDOR_IFD_TYPE   SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_INFO, 0x0101)
+//#define 	SCARD_ATTR_VENDOR_IFD_VERSION   SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_INFO, 0x0102)
+//	DWORD len;
+//	rv = SCardGetAttrib(hCard, SCARD_ATTR_VENDOR_IFD_SERIAL_NO, nil, &len);
+//	BYTE *buf = calloc(1,len);
+//	rv = SCardGetAttrib(hCard, SCARD_ATTR_VENDOR_IFD_SERIAL_NO, buf, &len);
+	
+	switch(dwActiveProtocol)
+	{
+		case SCARD_PROTOCOL_T0:
+			pioSendPci = *SCARD_PCI_T0;
+			break;
+			
+		case SCARD_PROTOCOL_T1:
+			pioSendPci = *SCARD_PCI_T1;
+			break;
+	}
+	
+	sendAPDU(hCard, pioSendPci, cmdSelectMGR, sizeof(cmdSelectMGR));
+	sendAPDU(hCard, pioSendPci, cmdGetConfig, sizeof(cmdGetConfig));	
+
+//	sendAPDU(hCard, pioSendPci, cmdPIVGetSerial, sizeof(cmdPIVGetSerial));
+
+#ifdef SCARD_AUTOALLOCATE
+	rv = SCardFreeMemory(hContext, mszReaders);
+	CHECK("SCardFreeMemory", rv)
+	
+#else
+	free(mszReaders);
+#endif
+	
+	rv = SCardReleaseContext(hContext);
 }
 
 - (NSDictionary*)getAnySingleDevice {
@@ -53,7 +166,7 @@ void IOServiceMatchedCallback(void* refcon, io_iterator_t iterator);
 	CFRunLoopAddSource(runLoop, loopSource, kCFRunLoopDefaultMode);
 
 	CFMutableDictionaryRef matchDict = (__bridge_retained CFMutableDictionaryRef)@{
-		@kIOProviderClassKey : @kIOUSBDeviceClassName,
+		@kIOProviderClassKey : @"IOUSBHostDevice",//@kIOUSBDeviceClassName,
 		@kUSBProductID : @"*",
 		@kUSBVendorID : @0x1050
 	};
@@ -117,6 +230,11 @@ CFDictionaryRef GetDeviceInfo(io_service_t usbDevice) {
 	kern_return_t kr;
 	CFMutableDictionaryRef devDict = nil;
 
+	io_registry_entry_t child;
+	CFMutableDictionaryRef dict = nil;
+	kr = IORegistryEntryGetChildEntry(usbDevice, kIOServicePlane, &child);
+	kr = IORegistryEntryCreateCFProperties(child, &dict, kCFAllocatorDefault, kNilOptions);
+
 	kr = IORegistryEntryCreateCFProperties(usbDevice, &devDict, kCFAllocatorDefault, kNilOptions);
 	if(kr!=KERN_SUCCESS) return nil;
 
@@ -129,6 +247,11 @@ CFDictionaryRef GetDeviceInfo(io_service_t usbDevice) {
 		CFDictionarySetValue(devDict, CFSTR(kUSBSerialNumberString), CFSTR("unknown"));
 	CFDictionarySetValue(devDict, (__bridge_retained CFStringRef)YubiKeyDeviceDictionaryUniqueStringKey,(__bridge CFStringRef)[gSelf getUniqueStringForDev:(__bridge NSDictionary*)devDict]);
 	
+	IOHIDDeviceRef hid = IOHIDDeviceCreate(kCFAllocatorDefault,usbDevice);
+	if(hid) {
+		IOReturn r = IOHIDDeviceOpen(hid, kIOHIDOptionsTypeNone);
+		r = IOHIDDeviceClose(hid, kIOHIDOptionsTypeNone);
+	}
 	return devDict;
 }
 
